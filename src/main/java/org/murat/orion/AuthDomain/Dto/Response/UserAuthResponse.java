cb6b69dddb5175a7b4753114f908d05e86ca3fa7
muratguven123 <muratguven376@gmail.com>
2026-01-15 17:44:43 +0300

    user auth contract tamamlandı

--- src/main/java/org/murat/orion/AuthDomain/Dto/Response/UserAuthResponse.java
+++ src/main/java/org/murat/orion/AuthDomain/Dto/Response/UserAuthResponse.java
@@ -1,4 +1,25 @@
 package org.murat.orion.AuthDomain.Dto.Response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserAuthResponse {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String role;
+    private Boolean isActive;
+    private Boolean isEmailVerified;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastLoginAt;
 }
