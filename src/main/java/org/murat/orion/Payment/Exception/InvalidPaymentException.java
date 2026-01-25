cde9373a8a38f32dabe70b96691bf14387070c52
muratguven123 <muratguven376@gmail.com>
2026-01-25 15:12:00 +0300

    PaymentNotFoundException ve InvalidPaymentException sınfılarını oluşturdum

---
+++ src/main/java/org/murat/orion/Payment/Exception/InvalidPaymentException.java
@@ -0,0 +1,8 @@
+package org.murat.orion.Payment.Exception;
+
+public class InvalidPaymentException extends PaymentException {
+
+    public InvalidPaymentException(String message) {
+        super(message);
+    }
+}
