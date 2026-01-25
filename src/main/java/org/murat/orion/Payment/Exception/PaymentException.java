36a036004166c3bf92ced76442940c049a7f2d69
muratguven123 <muratguven376@gmail.com>
2026-01-25 15:14:03 +0300

    PaymnetException sınıfı kalmıştı onuda yaptık :)

---
+++ src/main/java/org/murat/orion/Payment/Exception/PaymentException.java
@@ -0,0 +1,12 @@
+package org.murat.orion.Payment.Exception;
+
+public class PaymentException extends RuntimeException {
+
+    public PaymentException(String message) {
+        super(message);
+    }
+
+    public PaymentException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
