56ba2c9bdd7c97116ad0ce375bbb946bee79c581
muratguven123 <muratguven376@gmail.com>
2026-02-08 15:57:42 +0300

    notification service setted

---
+++ backend-microservices/notification-service/notification-service/src/test/java/com/org/orion/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.orion.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
