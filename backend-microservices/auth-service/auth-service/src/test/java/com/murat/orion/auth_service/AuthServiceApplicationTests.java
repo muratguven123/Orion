e859c576cbeef30738f9eeb4ec26e5bec9f60599
muratguven123 <muratguven376@gmail.com>
2026-02-19 18:32:03 +0300

    auht-service kurulumu yapıldı

---
+++ backend-microservices/auth-service/auth-service/src/test/java/com/murat/orion/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murat.orion.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
