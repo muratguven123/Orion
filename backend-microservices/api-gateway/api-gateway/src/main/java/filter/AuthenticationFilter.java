b636b93029bf83fb94773dcc6accb3efd0508ca4
muratguven123 <muratguven376@gmail.com>
2026-02-25 14:57:18 +0300

    add AuthenticationFilter to handle JWT authorization and user ID extraction

---
+++ backend-microservices/api-gateway/api-gateway/src/main/java/filter/AuthenticationFilter.java
@@ -0,0 +1,68 @@
+package filter;
+
+import Validator.RouteValidator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import util.JwtUtil;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    @Autowired
+    private RouteValidator validator;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            if (validator.isSecured.test(request)) {
+                if (!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    return onError(exchange, "Missing authorization header", HttpStatus.UNAUTHORIZED);
+                }
+                String authHeader = request.getHeaders().get(HttpHeaders.AUTHORIZATION).get(0);
+                if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                    authHeader = authHeader.substring(7);
+                } else {
+                    return onError(exchange, "Invalid authorization header format", HttpStatus.UNAUTHORIZED);
+                }
+                try {
+                    jwtUtil.validateToken(authHeader);
+
+                    String userId = jwtUtil.extractUserId(authHeader);
+                    request = exchange.getRequest()
+                            .mutate()
+                            .header("X-User-Id", userId)
+                            .build();
+
+                } catch (Exception e) {
+                    System.out.println("Invalid access... " + e.getMessage());
+                    return onError(exchange, "Unauthorized access to application", HttpStatus.UNAUTHORIZED);
+                }
+            }
+            return chain.filter(exchange.mutate().request(request).build());
+        });
+    }
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        exchange.getResponse().setStatusCode(httpStatus);
+        return exchange.getResponse().setComplete();
+    }
+
+    public static class Config {
+
+    }
+}
