ad043f5b83f025daa686e9a3d0381c1c026c01ba
muratguven123 <muratguven376@gmail.com>
2026-02-25 13:15:46 +0300

    add RouteValidator to manage open API endpoint access

---
+++ backend-microservices/api-gateway/api-gateway/src/main/java/Validator/RouteValidator.java
@@ -0,0 +1,23 @@
+package Validator;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class RouteValidator {
+    public static final List<String> openApiEndpoints=List.of(
+            "/api/auth/register",
+            "/api/auth/login/email",
+            "/api/auth/login/sms/send-otp",
+            "/api/auth/login/sms/verify",
+            "/api/auth/refresh-token",
+            "/euraka/**"
+    );
+    public Predicate<ServerHttpRequest> isSecured=
+            request ->openApiEndpoints
+                    .stream()
+                    .noneMatch(uri->request.getURI().getPath().contains(uri));
+}
