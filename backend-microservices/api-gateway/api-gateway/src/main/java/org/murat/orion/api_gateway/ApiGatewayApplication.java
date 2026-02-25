7977651c5981339fb4eee1883268cf13b1f9d31e
muratguven123 <muratguven376@gmail.com>
2026-02-25 14:57:00 +0300

    add component scan configuration for API gateway application

--- backend-microservices/api-gateway/api-gateway/src/main/java/org/murat/orion/api_gateway/ApiGatewayApplication.java
+++ backend-microservices/api-gateway/api-gateway/src/main/java/org/murat/orion/api_gateway/ApiGatewayApplication.java
@@ -3,9 +3,11 @@ package org.murat.orion.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@ComponentScan(basePackages = {"org.murat.orion.api_gateway", "filter", "util", "Validator"})
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
