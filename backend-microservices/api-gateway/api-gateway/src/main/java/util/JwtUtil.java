57ac8d780b44b6135908c849d8869bee8d4884ff
muratguven123 <muratguven376@gmail.com>
2026-02-25 13:34:50 +0300

    add JwtUtil class for JWT token handling and validation

---
+++ backend-microservices/api-gateway/api-gateway/src/main/java/util/JwtUtil.java
@@ -0,0 +1,40 @@
+package util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private Key getSigningKey() {
+        byte [] keyBytes= secret.getBytes();
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+    private Claims extractAllClaims(String token){
+        return io.jsonwebtoken.Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+    public <T> T extractClaim(String token, java.util.function.Function<Claims,T> claimsResolver){
+        final Claims claims=extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+        public boolean isTokenExpired(String token) {
+            return extractClaim(token, Claims::getExpiration).before(new java.util.Date());
+        }
+        public void validateToken(final String token) {
+            Jwts.parserBuilder().setSigningKey(getSigningKey()).build().parseClaimsJws(token);
+        }
+}
