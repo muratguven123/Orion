86a025961dd63081e7dcc052644dcd10ae583152
muratguven123 <muratguven376@gmail.com>
2026-03-05 14:57:28 +0300

    added
    rabbitmq config file added

--- backend-microservices/account-service/src/main/java/org/murat/accountservice/config/RabbitMQConfig.java
+++ backend-microservices/account-service/src/main/java/org/murat/accountservice/config/RabbitMQConfig.java
@@ -1,9 +1,8 @@
 package org.murat.accountservice.config;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -12,39 +11,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableRabbit
 public class RabbitMQConfig {
 
-    public static final String NOTIFICATION_QUEUE = "notification.queue";
     public static final String INTERNAL_EXCHANGE = "internal.exchange";
-    public static final String NOTIFICATION_ROUTING_KEY = "notification.#";
 
-    @Bean
-    public Queue notificationQueue() {
-        return new Queue(NOTIFICATION_QUEUE, true);
-    }
+    public static final String ROUTING_KEY_ACCOUNT_DEBITED = "notification.account.debited";
+    public static final String ROUTING_KEY_ACCOUNT_CREDITED = "notification.account.credited";
 
     @Bean
     public TopicExchange internalExchange() {
-        return new TopicExchange(INTERNAL_EXCHANGE);
+        return new TopicExchange(INTERNAL_EXCHANGE, true, false);
     }
 
     @Bean
-    public Binding binding(Queue notificationQueue, TopicExchange internalExchange) {
-        return BindingBuilder
-                .bind(notificationQueue)
-                .to(internalExchange)
-                .with(NOTIFICATION_ROUTING_KEY);
+    public MessageConverter jsonMessageConverter(ObjectMapper objectMapper) {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter(objectMapper);
+        converter.setDefaultCharset("UTF-8");
+        return converter;
     }
 
     @Bean
-    public MessageConverter jsonMessageConverter() {
-        return new Jackson2JsonMessageConverter();
-    }
-
-    @Bean
-    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
     }
 }
